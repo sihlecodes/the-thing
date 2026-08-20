0ee295e93503fc12d2291b62babec0e1d90fa7be
Siphesihle Mhlongo <sihlecodes@gmail.com>
2026-08-20 21:42:41 +0200

    implemented quiz screen
    fixed issues when reloading naration and quiz pages
    created new states

--- docs/main.js
+++ docs/main.js
@@ -1,6 +1,9 @@
 const States = Object.freeze({
     TTT_WON: 1,
-    TTT_DRAW: 2
+    TTT_DRAW: 2,
+    TTT_QUIZ: 3,
+    MAZE_COMPLETE: 4,
+    MAZE_QUIZ: 5,
 });
 
 async function loadPage(pageName) {
@@ -21,22 +24,27 @@ async function showNaration(heading, message, callback) {
     naration.setCallback(callback);
 };
 
-async function showQuiz(heading, message, correctAnswerCallback, wrongAnswerCallback) {
+async function showQuiz(heading, message, options, correctAnswerCallback, wrongAnswerCallback) {
     await loadPage('quiz');
 
     quiz.setHeading(heading);
     quiz.setMessage(message);
-    quiz.setOptions(['Option 1', 'Option 2', 'Option 3', 'Option 4']);
     quiz.setCorrectAnswerCallback(correctAnswerCallback);
     quiz.setWrongAnswerCallback(wrongAnswerCallback);
+
+    // should be called after setting callbacks
+    quiz.setOptions(options);
 };
 
 function getNextState(currentState) {
+    console.log('Current state:', currentState);
+
     switch (currentState) {
         case States.TTT_WON:
             showNaration('Congratulations', 'You have won the game of Tic Tac Toe', () => {
                 getNextState(States.TTT_QUIZ);
                 console.log('done!');
+                console.log('calling back from ttt won');
             });
 
             break;
@@ -49,13 +57,37 @@ function getNextState(currentState) {
             break;
 
         case States.TTT_QUIZ:
-            loadPage('quiz');
+            console.log('quiz time');
+            showQuiz('Tic Tac Toe Quiz',
+                'You have completed the game of Tic Tac Toe! Now answer this question to proceed.',
+                ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+                () => {
+                    loadPage('maze');
+                    console.log('Correct answer!');
+                },
+                () => {
+                    console.log('Wrong answer!');
+                    window.location.reload();
+                },
+            );
             break;
 
         case States.MAZE_COMPLETE:
+            console.log('maze complete');
             showNaration('Congratulations', 'You have completed the maze!', () => {
+                getNextState(States.MAZE_QUIZ);
                 console.log('done!');
             });
+            break;
+
+        case States.MAZE_QUIZ:
+            console.log('maze quiz time');
+            showQuiz('Maze Quiz',
+                'You have completed the maze! Now answer this question to proceed.',
+                ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+                () => { console.log('Correct answer!'); },
+                () => { console.log('Wrong answer!'); },
+            );
             break;
 
         default:
@@ -63,5 +95,18 @@ function getNextState(currentState) {
     }
 }
 
-// loadPage('tic-tac-toe');
-loadPage('maze');
+loadPage('tic-tac-toe');
+
+// showQuiz('Tic Tac Toe Quiz',
+//     'You have completed the game of Tic Tac Toe! Now answer this question to proceed.',
+//     ['Option 1', 'Option 2', 'Option 3', 'Option 4'],
+//     () => { console.log('Correct answer!'); },
+//     () => { console.log('Wrong answer!'); }
+// );
+
+// showNaration('Tic Tac Toe Quiz',
+//     'You have completed the game of Tic Tac Toe! Now answer this question to proceed.',
+//     () => { console.log('Correct answer!'); },
+// );
+
+// loadPage('maze');
