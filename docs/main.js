4f8c44d5df502befc0749d155602259c6c777e39
Siphesihle Mhlongo <sihlecodes@gmail.com>
2026-08-30 21:49:44 +0200

    thoughtful messages #3 done

--- docs/main.js
+++ docs/main.js
@@ -1,13 +1,16 @@
 const States = Object.freeze({
+    BEGIN: 0,
     TTT_WON: 1,
     TTT_DRAW: 2,
-    // TTT_QUIZ: 3,
+    PRE_MAZE: 3,
+    PRE_MAZE_TRANSITION: 5,
     TTT_LOST: 4,
     TTT_QUIZ_PASSED: 7,
 
     MAZE_COMPLETE: 8,
     MAZE_QUIZ_MESSAGE: 5,
     MAZE_QUIZ: 6,
+    MAZE_QUIZ_MESSAGE_2: 9
 });
 
 async function loadPage(pageName) {
@@ -20,7 +23,7 @@ async function loadPage(pageName) {
     content.appendChild(fragment);
 }
 
-async function showNarration({ hint = '', heading = '', message, callback, holdDuration = 1000 } = {}) {
+async function showNarration({ hint = '', heading = '', message, callback, holdDuration = 300 } = {}) {
     await loadPage('narration');
 
     if (heading.length > 0)
@@ -49,23 +52,23 @@ async function showQuiz({ heading = '', message, options, correctAnswerCallback,
 
 function getNextState(currentState) {
     console.log('Current state:', currentState);
-
     const refreshPage = () => window.location.reload();
 
     switch (currentState) {
-        case States.TTT_WON:
+        case States.BEGIN:
             showQuiz({
-                message: 'The day I first met you, you lent me something... what was it?',
+                message: 'The first time I met you was in WWG&nbsp;315. I asked the guys sitting next to me if I could borrow something, and from all the way across the room, you were the one who offered to lend me yours.<br><br>Do you remember what it was?',
                 options: ['A ruler', 'A pen', 'A calculator'],
 
                 correctAnswerCallback: () => {
-                    getNextState(States.TTT_QUIZ_MESSAGE);
+                    loadPage('tic-tac-toe');
+                    // getNextState(States.TTT_QUIZ_PASSED);
                 },
 
                 wrongAnswerCallback: () => {
                     showNarration({
-                        hint: 'Tap and hold the screen if you want to try again.',
                         message: 'That\'s not right. 😭',
+                        hint: 'Tap and hold the screen if you want to try again.',
                         callback: refreshPage
                     });
                 }
@@ -75,8 +78,8 @@ function getNextState(currentState) {
 
         case States.TTT_LOST:
             showNarration({
-                hint: 'Tap and hold the screen if you want to try again.',
                 message: 'It\'s actually impressive that you managed to lose considering that I intentionally programmed the AI to let you win. 😭🤣',
+                hint: 'Tap and hold the screen if you want to try again.',
                 callback: refreshPage
             });
 
@@ -84,48 +87,94 @@ function getNextState(currentState) {
 
         case States.TTT_DRAW:
             showNarration({
-                hint: 'Tap and hold the screen if you want to try again.',
                 message: 'How did you even manage to get a draw? I literally programmed the AI to let you win. 😭🤣',
+                hint: 'Tap and hold the screen if you want to try again.',
                 callback: refreshPage
             });
 
             break;
 
-        case States.TTT_QUIZ_PASSED:
+        case States.TTT_WON:
             showNarration({
-                message: 'On my way back from getting the ruler from you, I remember thinking about how stunning you were. When I got back to my seat, all I could think about was how I should have asked your name.',
-                callback: () => { loadPage('maze'); }
+                message: 'After you handed me the ruler, I remember just thinking about how stunning you looked. I told myself I\'d ask for your name when I return the ruler.<br><br>...I had it all planned out.',
+                callback: () => {
+                    showNarration({
+                        message: '...but you ended up leaving before I did, and that caught me so off guard that I returned the ruler without even remembering to ask your name. 🥲',
+                        callback: () => {
+                            getNextState(States.PRE_MAZE_TRANSITION);
+                        }
+                    });
+                }
+            });
+            break;
+
+        case States.PRE_MAZE_TRANSITION:
+            showNarration({
+                message: 'A week or two passsed...',
+                callback: () => {
+                    getNextState(States.PRE_MAZE);
+                }
+            });
+            break;
+
+        case States.PRE_MAZE:
+            showNarration({
+                message: 'The second time I saw you, you were standing in the middle of a corridor in WWG, waiting for a lecturer to pass by so they could let you through to the other side of the building.',
+
+                callback: () => {
+                    showQuiz({
+                        message: 'Do you remember which room it was that you were trying to get to?',
+                        options: ['WWG 112', 'WWG 113', 'WWG 226', 'WWG 223'],
+                        correctAnswerCallback: () => {
+                            showNarration({
+                                message: 'I had spotted you on my way down the stairs, and we waved at each other. I kept walking, and I was almost out of the building when it hit me:<br><br>"this might be my only chance in a while to talk to her."',
+                                callback: () => {
+                                    showNarration({
+                                        message: 'So I turned around and walked back up, and you were still there. Turns out you didn\'t know there was another way around to that side of the building, so I offered to walk you there myself.',
+                                        callback: () => {
+                                            loadPage('maze');
+                                        }
+                                    });
+                                }
+                            });
+
+                        },
+
+                        wrongAnswerCallback: () => {
+                            showNarration({
+                                hint: 'Tap and hold the screen if you want to try again.',
+                                message: 'That\'s not right. 😭',
+                                callback: refreshPage
+                            });
+                        }
+                    });
+                }
             });
             break;
 
         case States.MAZE_COMPLETE:
-            getNextState(States.MAZE_QUIZ);
+            getNextState(States.MAZE_QUIZ_MESSAGE_2);
             break;
 
-        case States.MAZE_QUIZ:
-            showQuiz({
-                message: 'The second time I saw you, you were standing in the middle of the corridor, hoping that a lecturer would show up and open the door that leads to which room?',
-                options: ['WWG 112', 'WWG 113', 'WWG 226', 'WWG 223'],
-
-                correctAnswerCallback: () => {
+        case States.MAZE_QUIZ_MESSAGE_2:
+            showNarration({
+                message: 'On the way, I asked what your name was and after that, you said something I still remember:<br><br>"I feel like WWG is a puzzle."',
+                callback: () => {
                     showNarration({
-                        hint: 'If you tap and hold one last time, I\'ve linked my WhatsApp, so you can send me a text message if you want to.',
-                        message: 'So far the only things I know about you is your name, that you own a ruler 😂, and that WWG is a like a "puzzle" to you (you\'re not alone in thinking that 😭) ...I would love to know more about you.',
+                        message: 'When we got there, you said, "Thanks, Sihle"... and I remember thinking my name had never sounded that beautiful before.<br><br>There was just something in the way you said it.',
                         callback: () => {
-                            window.location.href = 'https://wa.me/27633994508?text=Hello Sihle';
-                            console.log('Correct answer!')
-                        }
-                    });
-                },
-
-                wrongAnswerCallback: () => {
-                    showNarration({
-                        heading: 'Wrong Answer',
-                        message: 'You have answered the quiz incorrectly. Please try again.',
-                        callback: refreshPage
+                            showNarration({
+                                hint: 'I\'ve linked my WhatsApp, so if you\'d like, tap and hold one last time to send me a message.',
+                                message: 'It\'s funny that, after all that, the only things I actually know about you are your name, that you own a ruler 😂, and that WWG feels like a puzzle to you (you\'re not alone in that)<br><br>...I\'d love to know more about you.',
+                                callback: () => {
+                                    window.location.href = 'https://wa.me/27633994508?text=Hello Sihle';
+                                }
+                            });
+                        },
                     });
                 }
             });
+        case States.MAZE_QUIZ:
             break;
 
         case States.MAZE_QUIZ_MESSAGE:
@@ -146,7 +195,8 @@ function getNextState(currentState) {
 }
 
 // loadPage('tic-tac-toe');
-// getNextState(States.MAZE_QUIZ);
+getNextState(States.BEGIN);
+// getNextState(States.MAZE_COMPLETE);
 
 // showQuiz('Tic Tac Toe Quiz',
 //     'You have completed the game of Tic Tac Toe! Now answer this question to proceed.',
@@ -160,4 +210,4 @@ function getNextState(currentState) {
 //     () => { console.log('Correct answer!'); },
 // );
 
-loadPage('maze');
+// loadPage('maze');
